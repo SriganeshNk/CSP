d419c2188409d01f8f55eaf3584c3b7f56be4cdd
srnavaneetha <sriganesh1991@gmail.com>
2014-10-16 16:52:13 -0400

    added my backtracking implementation

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -19,43 +19,54 @@ from sudokuUtil import *
 # You can utilize argv to distinguish between algorithms
 # (basic backtracking or with MRV and forward checking).
 # For example: python sudokuSolver.py backtracking
+MRV = True
+NoMRV = False
+Simple = True
+NoSimple = False
+checkSolution = True
+simpleCheck = False
+
 def getUnit(row, col):
-        startR = row - (row%3)
-        startC = col - (col%3)
-        return startR, startR+2, startC, startC+2
+	startR , startC, endR, endC = 0, 0, 0, 0
+	if (row+1)%3 == 0:
+		startR, endR = row-2, row
+	if (col+1)%3 == 0:
+		startC, endC = col-2, col
+	if (row+1)%3 == 1:
+		startR, endR = row, row+2
+	if (col+1)%3 == 1:
+		startC, endC = col, col+2
+	if (row+1)%3 == 2:
+		startR, endR = row-1, row+1
+	if (col+1)%3 == 2:
+		startC, endC = col-1, col+1
+	return startR, endR, startC, endC
 
-def replace_l(lst, x ,y):
-	lst = lst.replace(x, y)
-
-import pdb
-
-# Check for numbers in row, col and unit
+#Forward Checking
 def suggestValue(tempPuzzle, row, col, elimTry):
 	possibleValues = '123456789'
-        
-	#Check for numbers in row
-	[replace_l(possibleValues, x, ' ') for x in possibleValues if x in tempPuzzle[row]]
-        
-	# check for numbers in col
-	[replace_l(possibleValues, tempPuzzle[x][col], ' ') for x in col_len if (tempPuzzle[x][col] != '0') and len(tempPuzzle[x][col]) == 1]
-
+	#Row Check
+	#pdb.set_trace()
+	for x in possibleValues:
+		if tempPuzzle[row].count(x):
+			possibleValues = possibleValues.replace(x,'')
+	#col Check
+	colValues = []
+	for x in range(len(tempPuzzle[col])):
+		if tempPuzzle[x][col] != '0' and len(tempPuzzle[x][col]) == 1:
+			colValues.append(tempPuzzle[x][col])
+	for x in possibleValues:
+		if colValues.count(x):
+			possibleValues = possibleValues.replace(x,'')
+	#3*3 check
+	#calculating the box that this value belongs to.
 	startR, endR ,startC, endC = getUnit(row, col)
- 
-	# check for numbers in the unit
-	[replace_l(possibleValues, tempPuzzle[x][y], ' ') for x in range(startR, endR+1) 
-                       for y in range(startC, endC+1) if (tempPuzzle[x][y] != '0') and len(tempPuzzle[x][y]) == 1]
-
+	for x in range(startR, endR+1):
+		for y in range(startC, endC+1):
+			if tempPuzzle[x][y] != '0' and len(tempPuzzle[x][y]) == 1:
+				possibleValues = possibleValues.replace(tempPuzzle[x][y],'')
 	if len(possibleValues) == 1 or elimTry == 1:
-                tempPuzzle[row][col] = possibleValues
-
-def isSolved(dummyPuzzle):
-	solved = False
-	for x in row_len:
-		for y in col_len:
-			solved = checkCorrectness(dummyPuzzle, x, y, True)
-			if solved == False:
-				return False
-	return solved
+		tempPuzzle[row][col] = possibleValues
 
 # see if the tried value confirms to the games rules
 def checkCorrectness(dummyPuzzle, row, col, strict=False):
@@ -95,18 +106,36 @@ def checkCorrectness(dummyPuzzle, row, col, strict=False):
 			return False
 		if unitList.count(i) > 1:
 			return False
-	dummyPuzzle[row] = rowList
-	for i in range(len(dummyPuzzle[col])):
-		dummyPuzzle[i][col] = colList[i]
-	i = 0
-	for x in range(startR, endR+1):
-		for y in range(startC, endC+1):
-			dummyPuzzle[x][y]=unitList[i]
-			i = i+1
+
+	if strict == False:
+		dummyPuzzle[row] = rowList
+		for i in range(len(dummyPuzzle[col])):
+			dummyPuzzle[i][col] = colList[i]
+		i = 0
+		for x in range(startR, endR+1):
+			for y in range(startC, endC+1):
+				dummyPuzzle[x][y]=unitList[i]
+				i = i+1
+		return True
 	return True
+
+def isSolved(dummyPuzzle):
+	solved = False
+	for x in range(len(dummyPuzzle)):
+		for y in range(len(dummyPuzzle[x])):
+			solved = checkCorrectness(dummyPuzzle, x, y, checkSolution)
+			if solved == False:
+				return False
+	return solved
 	
-def nextEntry(strPuzzle, MRV=False):
-	if MRV:
+def nextEntry(strPuzzle, MRVHeuristic=False, ZERO=False):
+	if ZERO:
+		for row in range(len(strPuzzle)):
+			for col in range(len(strPuzzle[row])):
+				if strPuzzle[row][col] == '0':
+					return row, col
+		return -1, -1
+	if MRVHeuristic:
 		r, c = 0, 0
 		minPossible = int(11)
 		for row in range(len(strPuzzle)):
@@ -128,66 +157,77 @@ def revert(dummyPuzzle, strPuzzle):
 		for col in range(len(strPuzzle[row])):
 			dummyPuzzle[row][col] = strPuzzle[row][col]
 
+def simpleSearch(strPuzzle):
+	"Using depth-first search and propagation, try all possible values."
+	#pdb.set_trace()
+	row, col = nextEntry(strPuzzle, NoMRV, Simple)
+	possibleValues = '123456789'
+	solved = False
+	for x in possibleValues:
+		strPuzzle[row][col] = x
+		if checkCorrectness(strPuzzle, row, col, simpleCheck) == True:
+			if isSolved(strPuzzle):
+				return (strPuzzle, True)
+			dummyPuzzle, solved = simpleSearch(copy.deepcopy(strPuzzle))
+			if solved:
+				return (dummyPuzzle, solved)
+			else:
+				revert(dummyPuzzle, strPuzzle)
+	if solved:
+		return (strPuzzle, True)
+	else:
+		return (strPuzzle, False)
 
 def search(dummyPuzzle, strPuzzle, row, col):
 	"Using depth-first search and propagation, try all possible values."
-	if len(strPuzzle[row][col]) == 1: 
-		return True
 	tryNum = len(strPuzzle[row][col])-1
-	
+	solved = False
 	while tryNum > -1:
 		temp =  str(strPuzzle[row][col][tryNum])
 		dummyPuzzle[row][col] = temp
-		if checkCorrectness(dummyPuzzle, row, col, False) == False:
-			revert(dummyPuzzle, strPuzzle)
-		else:
-			newR, newC = nextEntry(dummyPuzzle, True)
+		if checkCorrectness(dummyPuzzle, row, col, simpleCheck) == True:
+			newR, newC = nextEntry(dummyPuzzle, MRV)
 			if newR == -1 and newC == -1:
 				if isSolved(dummyPuzzle):
-					return dummyPuzzle
-			dummyPuzzle = search(dummyPuzzle, copy.deepcopy(dummyPuzzle), newR, newC)
-			if isSolved(dummyPuzzle):
-				return dummyPuzzle
+					return (dummyPuzzle, True)
+			dummyPuzzle, solved = search(dummyPuzzle, copy.deepcopy(dummyPuzzle), newR, newC)
+			if solved:
+				return (dummyPuzzle, True)
 			else:
 				revert(dummyPuzzle, strPuzzle)
 		tryNum = tryNum - 1	
-	#print "------Dummy Puzzle--------------"
-	#display(dummyPuzzle)
-	#print "-------Str Puzzle-------------"
-	#display(strPuzzle)
-	if isSolved(dummyPuzzle):
-		return dummyPuzzle
+	if solved:
+		return (dummyPuzzle, True)
 	else:
-		return strPuzzle
-
+		return (strPuzzle, False)
 
 def solve_puzzle(puzzle, argv):
 	"""Solve the sudoku puzzle."""
-        strPuzzle = [map(str, x) for x in puzzle]
-
-	global row_len
-        row_len = range(len(strPuzzle))
-        global col_len
-        col_len = range(len(strPuzzle[0]))
-
-        [suggestValue(strPuzzle,row,col,i) for i in range(2)
-                for row in row_len for col in col_len if strPuzzle[row][col] == '0']
-
-	startR, startC = nextEntry(strPuzzle, True)
-	dummyPuzzle = copy.deepcopy(strPuzzle)
-	dummyPuzzle = search(dummyPuzzle, strPuzzle, startR, startC)
-	#print "------original puzzle------------"
-	#display(puzzle)
-	#print "------converted puzzle-----------"
-	#display(strPuzzle)
-	#print "------solved puzzle---------------"
-	display(dummyPuzzle)
+	strPuzzle = []
+	dummyPuzzle = []
+	solved = False
+	for x in puzzle:
+		temp = map(str, x)
+		strPuzzle.append(temp)
+	print argv
+	if len(argv) > 1 and argv[1] == "backtracking":
+		dummyPuzzle, solved = simpleSearch(strPuzzle)
+	else:
+		for i in range(2):
+			for row in range(len(strPuzzle)):
+				for col in range(len(strPuzzle[row])):
+					if strPuzzle[row][col] == '0':
+						suggestValue(strPuzzle, row, col, i)
+		startR, startC = nextEntry(strPuzzle, True)
+		dummyPuzzle = copy.deepcopy(strPuzzle)
+		dummyPuzzle , solved = search(dummyPuzzle, strPuzzle, startR, startC)
+		
 	result = []
-	if isSolved(dummyPuzzle):
-		[result.append(map(int, x)) for x in dummyPuzzle]
-		#for x in dummyPuzzle:
-		#	temp = map(int, x)
-		#	result.append(temp)	
+	if solved:
+		display(dummyPuzzle)
+		for x in dummyPuzzle:
+			temp = map(int, x)
+			result.append(temp)	
 		return result
 
 	return load_sudoku('given_solution.txt')
@@ -202,4 +242,3 @@ t1 = time()
 print "completed. time usage: %f" %(t1 - t0), "secs."
 
 save_sudoku('solution.txt', solution)
-
